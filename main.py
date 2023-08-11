a0537456632439d8907079a38d3995d8deea5d49
azrael3kchh <141904569+azrael3kchh@users.noreply.github.com>
2023-08-11 07:17:25 +0000

    asdf

--- main.py
+++
@@ -1,7 +0,0 @@
-echo "# Web" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/azrael3kchh/Web.git
-git push -u origin main
