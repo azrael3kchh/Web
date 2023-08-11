d7926813bce9d5481b37d5c850483eb9fda379a8
azrael3kchh <141904569+azrael3kchh@users.noreply.github.com>
2023-08-11 07:15:03 +0000

    sdfasf

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,29 @@
-from django.db import models
+"""
+參照Django文檔
+编写你的第一个 Django 应用，第 2 部分
+測試class
+cd workspaces/Web/mysite
+"""py
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
+    """Question"""
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-
+    def __str__(self):
+        """待補充"""
+        return self.question_text
+    def was_published_recently(self):
+        """待補充"""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
+    """Choice"""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
--- readme.md
+++ readme.md
@@ -1,14 +1,32 @@
-# 安裝模組
-$pip install django 
-# 查看版本
+# 编写你的第一个 Django 应用，第 1 部分¶
+__安裝模組__
+(ˋ)$pip install django (ˊ)
+**查看版本**
+```python
 $python -m django --version
-# 移到指定路徑位置
+```
+** 移到指定路徑位置**
 ! cd /content/drive/MyDrive/"Colab Notebooks"/Web #這段無法執行，似乎預設路徑就是暫存路徑
-# 開發專案
+##用于开发的简易服务器¶
+**開發專案**
+```python
 $ django-admin startproject mysite
-# 移至專案位置
-$ cd mysite
-$ python manage.py runserver
-# 若想更换服务器的监听端口，可用以下
-$ python manage.py runserver 8080
+```
+**移至專案位置**
+```python
+ - cd mysite
+- $ python manage.py runserver
+ ```
+- **若想更换服务器的监听端口，可用以下**
+```python
+- $ python manage.py runserver 8080
+ ```
 
+# 编写你的第一个 Django 应用，第 2 部分
+
+**尝试一下 Django 为你创建的各种 API**
+```python
+$ python manage.py shell
+ ```
+
+默认开启的某些应用需要至少一个数据表，所以，在使用他们之前需要在数据库中创建一些表。请执行以下命令：
