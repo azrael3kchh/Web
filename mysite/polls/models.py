ef24ffdfbe32831a0a5e00064292500441428796
azrael3kchh <141904569+azrael3kchh@users.noreply.github.com>
2023-08-11 08:46:38 +0000

    sdfs

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 """
 參照Django文檔
 编写你的第一个 Django 应用，第 2 部分
 測試class
-cd workspaces/Web/mysite
-"""py
-=======
-'''
-終端機測試
-cd azrael3kchh/Web/mysite/mysite
-'''
->>>>>>> 3c086f21d26fbde5919e256974eec7bdea924b38
+cd mysite
+python manage.py shell
+from polls.models import Choice, Question
+"""
 import datetime
 
 from django.db import models
@@ -21,15 +16,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     def __str__(self):
-<<<<<<< HEAD
         """待補充"""
         return self.question_text
     def was_published_recently(self):
-        """待補充"""
-=======
-        return self.question_text
-    def was_published_recently(self):
->>>>>>> 3c086f21d26fbde5919e256974eec7bdea924b38
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
@@ -38,8 +27,4 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-<<<<<<< HEAD
         return self.choice_text
-=======
-        return self.choice_text
->>>>>>> 3c086f21d26fbde5919e256974eec7bdea924b38
--- readme.md
+++ readme.md
@@ -1,37 +1,4 @@
 # 编写你的第一个 Django 应用，第 1 部分¶
-<<<<<<< HEAD
-__安裝模組__
-(ˋ)$pip install django (ˊ)
-**查看版本**
-```python
-$python -m django --version
-```
-** 移到指定路徑位置**
-! cd /content/drive/MyDrive/"Colab Notebooks"/Web #這段無法執行，似乎預設路徑就是暫存路徑
-##用于开发的简易服务器¶
-**開發專案**
-```python
-$ django-admin startproject mysite
-```
-**移至專案位置**
-```python
- - cd mysite
-- $ python manage.py runserver
- ```
-- **若想更换服务器的监听端口，可用以下**
-```python
-- $ python manage.py runserver 8080
- ```
-
-# 编写你的第一个 Django 应用，第 2 部分
-
-**尝试一下 Django 为你创建的各种 API**
-```python
-$ python manage.py shell
- ```
-
-默认开启的某些应用需要至少一个数据表，所以，在使用他们之前需要在数据库中创建一些表。请执行以下命令：
-=======
 ## 安裝模組
 ```python
 $pip install django
@@ -61,4 +28,3 @@ $ python manage.py startapp polls
 ```python
 $ python manage.py migrate
 ```
->>>>>>> 3c086f21d26fbde5919e256974eec7bdea924b38
